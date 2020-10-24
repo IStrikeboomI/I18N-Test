e78002de35d0d8308999b3d5d6c2c887c1eebfbd
IStrikeboomI <shubhmakadia12@gmail.com>
2020-10-23 22:45:23 -0500

    Change a contains to a start with

--- src/Strikeboom/i18n_test/I18N/I18N.java
+++ src/Strikeboom/i18n_test/I18N/I18N.java
@@ -8,6 +8,6 @@ import java.io.InputStreamReader;
 public class I18N {
     public static String format(String key) {
         return new BufferedReader(new InputStreamReader(I18N.class.getResourceAsStream("/Strikeboom/i18n_test/resources/" + Main.getLanguage() + ".txt")))
-                .lines().filter(s -> s.contains(key) && s.contains("=") && !s.contains("//")).findFirst().map(s -> s.substring(s.lastIndexOf("=") + 1)).orElse(key);
+                .lines().filter(s -> s.contains(key) && s.contains("=") && !s.startsWith("//")).findFirst().map(s -> s.substring(s.lastIndexOf("=") + 1)).orElse(key);
     }
 }
